27a98fbb2682f05cd75067afefff68aa80db09ab
Anas <anastiour@gmail.com>
2023-04-09 02:02:32 +0000

    Require the CreateSignedTransaction trait by the template pallet

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -18,6 +18,7 @@ mod benchmarking;
 pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+	use frame_system::offchain::CreateSignedTransaction;
 	use frame_support::log;
 
 	#[pallet::pallet]
@@ -25,7 +26,8 @@ pub mod pallet {
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
-	pub trait Config: frame_system::Config {
+	// pub trait Config: frame_system::Config {
+	pub trait Config: CreateSignedTransaction<Call<Self>> + frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
